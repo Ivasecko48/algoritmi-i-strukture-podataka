755fc38d2b69f5bb7874bb25ce58eebad5131f50
ivasecko48 <robertivasecko2001@gmail.com>
2024-10-29 11:57:18 +0100

    2nd

---
+++ Vj01/Comparers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Comparers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students =
+            {
+                new Student("Ivo", 4.1),
+                new Student("Ana", 4.9),
+                new Student("Iva", 4.3),
+                new Student("Bob", 4.5),
+                new Student("Joe", 4.7)
+            };
+
+            Console.WriteLine("Studenti prije:");
+			 foreach (Student i in students)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+            StudentComparer comparer = new StudentComparer(StudentComparerType.Name);
+
+            Bubble.Sort(students, comparer);
+
+            Console.WriteLine("Studenti sortirani po imenu:");
+			 foreach (Student i in students)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+            comparer = new StudentComparer(StudentComparerType.Grade);
+
+            Bubble.Sort(students, comparer);
+
+			Console.WriteLine("Studenti sortirani po ocjeni:");
+			 foreach (Student i in students)
+            {
+                Console.WriteLine(i.ToString());
+            }
+        }
+    }
+}
---
+++ Vj01/Comparers/Student.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparers
+
+{
+    public class Student
+    {
+        private string name;
+        private double grade;
+
+        public Student(string name, double grade) { this.name = name; this.grade = grade; }
+
+        public override string? ToString()
+        {
+            return name;
+        }
+    }
+}
